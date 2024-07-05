e6bd00a5bff154128bb9dbd1ec91a635dac2cea9
Raajh <raajhganshekar@gmail.com>
2024-07-05 13:38:20 +0530

    Updated App.js to add new feature

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, World,its friday and weekend and holiday
-        
+      <h1>Hello, World,its friday and weekend and holiday and bye
+
       </h1>
     </div>
   );
