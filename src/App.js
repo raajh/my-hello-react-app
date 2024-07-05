2772faeac3a7117e98e292c67ce62523140da8e9
Raajh <raajhganshekar@gmail.com>
2024-07-05 15:14:35 +0530

    Add app.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, World,its friday and weekend and holiday and bye,Hello....jjj
+      <h1>Hello, World,its friday and weekend and holiday and bye,Hello....jjjwqwq
 
       </h1>
     </div>
