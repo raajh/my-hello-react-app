8c237fc5e0754b5f087668b8607edfa9b7dcfe54
Raajh <raajhganshekar@gmail.com>
2024-07-05 14:55:51 +0530

    updated app.js file2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, World,its friday and weekend and holiday and bye,Hello....
+      <h1>Hello, World,its friday and weekend and holiday and bye,Hello....jjj
 
       </h1>
     </div>
